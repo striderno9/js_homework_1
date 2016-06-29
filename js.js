91dde3612ce2107d7a297831fd77e664d0b99623
Jason Almenas <i.striderno9@icloud.com>
2016-06-29 13:12:43 -0400

    added files to folder

---
+++ js.js
@@ -0,0 +1,14 @@
+var games = ["Space Invaders", "Tetris", "Donkey Kong", "Pong", "Breakout!"];
+var movies = ["Halloween", "Fight Club", "Donnie Darko", "Glory" ];
+var heroes = ["Spider-Man", "Batman", "Superman", "Captain America"];
+
+console.log(games[1]);
+console.log(movies[2]);
+console.log(heroes[0]);
+
+alert("Hello World");
+
+var city = ["New York", "Cleveland", "Chicago"];
+
+console.log("My favorite city is " + city[0]);
+
