b7d92f4c0f46ea2df3022df5c175c5f8d707b0d1
Jason Almenas <i.striderno9@icloud.com>
2016-06-30 13:26:02 -0400

    changed folder structure

---
+++ javascript/js.js
@@ -0,0 +1,29 @@
+var games = ["Space Invaders", "Tetris", "Donkey Kong", "Pong", "Breakout!"];
+var movies = ["Halloween", "Fight Club", "Donnie Darko", "Glory" ];
+var heroes = ["Spider-Man", "Batman", "Superman", "Captain America"];
+
+console.log(games[1]);
+console.log(movies[2]);
+console.log(heroes[0]);
+
+alert("Hello World");
+
+var city = ["New York", "Cleveland", "Chicago"];
+
+console.log("My favorite city is " + city[0]);
+
+var blue = 19;
+var red = 22;
+var yellow = 100;
+
+console.log(yellow - blue);
+
+// This is an example of a comment in js
+
+// And this is 
+// also an example
+// of a comment on multiple lines
+
+function LogToHTML() {
+	document.getElementById("change").innerHTML = "Told ya!";
+}
